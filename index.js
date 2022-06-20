e755db701d437989c59b639312a0e773aec0bde8
allanstar-byte <allanmutisya263@gmail.com>
2022-06-20 04:52:18 +0300

    Palidrome

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+    let reversed = word.split("").reverse().join("")
+    if (word === reversed) {
+        console.log('true')
+    } else {
+        console.log('false')
+    }
 }
 
-/* 
-  Add your pseudocode here
-*/
+isPalindrome('abba')
+isPalindrome('racecar')
+isPalindrome('a')
+isPalindrome('robot')
+isPalindrome('ab')
+    /* 
+      Add your pseudocode here
+
+      1. I start by splitting the characters with spaces
+      2. reversig the characters 
+      3. Joining the reversed characters and assing to a new variable
+      4. writting a conditional statement
+
+    */
 
 /*
   Add written explanation of your solution here
@@ -12,14 +29,14 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
