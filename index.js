4c965b648b349e564daf266b7df39095e934906d
allanstar-byte <allanmutisya263@gmail.com>
2022-06-20 05:18:17 +0300

    solution 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// SOLUTION 1
+
 function isPalindrome(word) {
     // Write your algorithm here
     let reversed = word.split("").reverse().join("")
@@ -13,9 +15,29 @@ isPalindrome('racecar')
 isPalindrome('a')
 isPalindrome('robot')
 isPalindrome('ab')
+
+
+// SOLUTION 2
+
+function isPalindrome(word) {
+    // iterate from the beginning of the string to the middle of the string
+    for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+        // compare the letter we're iterating over to the corresponding letter at the end of the string
+        const endIndex = word.length - 1 - startIndex;
+        if (word[startIndex] !== word[endIndex]) {
+            // if the letters don't match, return false
+            return false;
+        }
+    }
+
+    // if we reach the middle, and all the letters match, return true
+    return true;
+}
+
+isPalindrome('allan')
     /* 
       Add your pseudocode here
-      
+
       if the reversed word is strictly equal to the passed word,
       then return true
 
