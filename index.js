2b298d5ae1b5d61106e2a1be4516c4c14841283b
allanstar-byte <allanmutisya263@gmail.com>
2022-06-20 05:11:13 +0300

    Palidromem

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function isPalindrome(word) {
     // Write your algorithm here
     let reversed = word.split("").reverse().join("")
     if (word === reversed) {
-        console.log('true')
+        console.log(true)
     } else {
-        console.log('false')
+        console.log(false)
     }
 }
 
@@ -15,16 +15,22 @@ isPalindrome('robot')
 isPalindrome('ab')
     /* 
       Add your pseudocode here
+      
+      if the reversed word is strictly equal to the passed word,
+      then return true
 
-      1. I start by splitting the characters with spaces
-      2. reversig the characters 
-      3. Joining the reversed characters and assing to a new variable
-      4. writting a conditional statement
+      else return false
 
     */
 
 /*
   Add written explanation of your solution here
+
+
+      1. I start by splitting the characters with spaces
+      2. reversig the characters 
+      3. Joining the reversed characters and assing to a new variable
+      4. writting a conditional statement
 */
 
 // You can run `node index.js` to view these console logs
